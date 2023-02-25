732823b2f656275dd5687196ff2deeb7c3395f3e
Anwar <anwarjahid449@gmail.com>
2023-02-25 23:54:29 +0600

    phone hunter

---
+++ js/phone.js
@@ -0,0 +1,27 @@
+const loadPhones = async() =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones =>{
+    phones.forEach(phone =>{
+        console.log(phone);
+        const phonesContainer = document.getElementById('phones-container');
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+        `;
+        phonesContainer.appendChild(div);
+    })
+}
+
+loadPhones();

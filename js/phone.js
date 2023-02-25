e65dd6e084a63393a7d913393bad09e8bdd1be48
Anwar <anwarjahid449@gmail.com>
2023-02-26 00:36:52 +0600

    searchText

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,20 @@
-const loadPhones = async() =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async(searchText) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data);
 }
 
 const displayPhones = phones =>{
+    const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = '';
     phones.forEach(phone =>{
         console.log(phone);
-        const phonesContainer = document.getElementById('phones-container');
+        
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div class="card p-5">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -23,5 +25,11 @@ const displayPhones = phones =>{
         phonesContainer.appendChild(div);
     })
 }
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchText = document.getElementById('input-field').value;
+    loadPhones(searchText);
+    document.getElementById('input-field').value = '';
+})
 
 loadPhones();

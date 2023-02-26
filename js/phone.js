3cadcc9447c99ba688bfdc19c861611a7d858abb
Anwar <anwarjahid449@gmail.com>
2023-02-27 00:16:07 +0600

    showall array slice

--- js/phone.js
+++ js/phone.js
@@ -1,19 +1,46 @@
-const loadPhones = async(searchText) =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayPhones(data.data);
+const spinner = document.getElementById('spinner');
+function spinnerToggle(loadSpinner) {
+  if (loadSpinner) {
+    spinner.classList.remove('d-none');
+  } 
+  else {
+    spinner.classList.add('d-none');
+  }
 }
 
-const displayPhones = phones =>{
-    const phonesContainer = document.getElementById('phones-container');
-    phonesContainer.textContent = '';
-    phones.forEach(phone =>{
-        console.log(phone);
-        
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+const loadPhones = async (searchText, dataLimit) => { 
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhones(data.data, dataLimit);
+}
+
+const displayPhones = (phones, dataLimit) => {  
+  const phonesContainer = document.getElementById('phones-container');
+  phonesContainer.textContent = '';
+
+  const showAll = document.getElementById('show-all');
+  if(typeof dataLimit =='number' && phones.length > dataLimit){
+    phones = phones.slice(0, 5);
+    showAll.classList.remove('d-none');    
+  }
+  else{       
+    showAll.classList.add('d-none');
+    document.getElementById('input-field').value ='';
+  }
+
+  const errorMessage = document.getElementById('error-message');
+  if (phones.length === 0) {
+    errorMessage.classList.remove('d-none');
+  } else {
+    errorMessage.classList.add('d-none');
+  }  
+  
+  phones.forEach(phone => {
+    console.log(phone);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
         <div class="card p-5">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -22,14 +49,32 @@ const displayPhones = phones =>{
         </div>
       </div>
         `;
-        phonesContainer.appendChild(div);
-    })
+    phonesContainer.appendChild(div);
+  })
+  spinnerToggle(false);
+  
 }
 
-document.getElementById('btn-search').addEventListener('click', function(){
-    const searchText = document.getElementById('input-field').value;
-    loadPhones(searchText);
-    document.getElementById('input-field').value = '';
+function processSearch(dataLimit){  
+  const searchText = document.getElementById('input-field').value;
+  loadPhones(searchText,dataLimit);
+  spinnerToggle(true);  
+}
+
+document.getElementById('btn-search').addEventListener('click', function () {
+  processSearch(5);
+})
+document.getElementById('input-field').addEventListener('keyup', function (e) {
+  if(e.key === 'Enter'){
+    processSearch(5);
+  }
 })
 
-loadPhones();
+document.getElementById('btn-show-all').addEventListener('click', function () {
+  
+  processSearch();
+})
+
+loadPhones('samsung');
+
+
